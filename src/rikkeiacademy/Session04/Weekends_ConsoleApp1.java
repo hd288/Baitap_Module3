f37cc4d97c0117cdea81b97d8ea1a173c11789f8
hd288 <henrydinhjp288@gmail.com>
2023-09-15 17:35:37 +0700

    add bai cuoi tuan

---
+++ src/rikkeiacademy/Session04/Weekends_ConsoleApp1.java
@@ -0,0 +1,177 @@
+package rikkeiacademy.Session04;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Weekends_ConsoleApp1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Khai báo biến mảng và số phần tử
+        int[] arr = null;
+        int n = 0;
+
+        while (true) {
+            System.out.println("**********************MENU************************");
+            System.out.println("1. Nhập giá trị các phần tử mảng");
+            System.out.println("2. Hiển thị giá trị các phần tử mảng");
+            System.out.println("3. In các phần tử giá trị chẵn trong mảng và tính tổng");
+            System.out.println("4. In các phần tử có giá trị là số nguyên tố và tính tổng");
+            System.out.println("5. In ra phần tử có giá trị lớn nhất và nhỏ nhất trong mảng");
+            System.out.println("6. Sắp xếp mảng giảm dần");
+            System.out.println("7. Nhập vào giá trị số nguyên (m) và thống kê trong mảng có bao nhiêu phần tử có giá trị là m");
+            System.out.println("8. Thoát");
+            System.out.println("**************************************************");
+            System.out.print("Nhập lựa chọn của bạn: ");
+
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    // Nhập giá trị các phần tử mảng
+                    System.out.print("Nhập số phần tử của mảng: ");
+                    n = scanner.nextInt();
+                    arr = new int[n];
+                    for (int i = 0; i < n; i++) {
+                        System.out.print("Nhập phần tử thứ " + (i + 1) + ": ");
+                        arr[i] = scanner.nextInt();
+                    }
+                    break;
+                case 2:
+                    // Hiển thị giá trị các phần tử mảng
+                    if (arr != null) {
+                        System.out.print("Các phần tử của mảng: ");
+                        for (int i : arr) {
+                            System.out.print(i + " ");
+                        }
+                        System.out.println();
+                    } else {
+                        System.out.println("Mảng chưa được khởi tạo.");
+                    }
+                    break;
+                case 3:
+                    // In các phần tử giá trị chẵn và tính tổng
+                    if (arr != null) {
+                        int sumEven = 0;
+                        System.out.print("Các phần tử giá trị chẵn trong mảng: ");
+                        for (int i : arr) {
+                            if (i % 2 == 0) {
+                                System.out.print(i + " ");
+                                sumEven += i;
+                            }
+                        }
+                        System.out.println();
+                        System.out.println("Tổng các phần tử giá trị chẵn: " + sumEven);
+                    } else {
+                        System.out.println("Mảng chưa được khởi tạo.");
+                    }
+                    break;
+                case 4:
+                    // In các phần tử là số nguyên tố và tính tổng
+                    if (arr != null) {
+                        int sumPrime = 0;
+                        System.out.print("Các phần tử là số nguyên tố trong mảng: ");
+                        for (int i : arr) {
+                            if (isPrime(i)) {
+                                System.out.print(i + " ");
+                                sumPrime += i;
+                            }
+                        }
+                        System.out.println();
+                        System.out.println("Tổng các phần tử là số nguyên tố: " + sumPrime);
+                    } else {
+                        System.out.println("Mảng chưa được khởi tạo.");
+                    }
+                    break;
+                case 5:
+                    // In phần tử lớn nhất và nhỏ nhất
+                    if (arr != null) {
+                        int max = arr[0];
+                        int min = arr[0];
+                        for (int i : arr) {
+                            if (i > max) {
+                                max = i;
+                            }
+                            if (i < min) {
+                                min = i;
+                            }
+                        }
+                        System.out.println("Phần tử lớn nhất trong mảng: " + max);
+                        System.out.println("Phần tử nhỏ nhất trong mảng: " + min);
+                    } else {
+                        System.out.println("Mảng chưa được khởi tạo.");
+                    }
+                    break;
+                case 6:
+                    // Sắp xếp mảng giảm dần
+                    if (arr != null) {
+                        Arrays.sort(arr);
+                        reverseArray(arr);
+                        System.out.print("Mảng sau khi sắp xếp giảm dần: ");
+                        for (int i : arr) {
+                            System.out.print(i + " ");
+                        }
+                        System.out.println();
+                    } else {
+                        System.out.println("Mảng chưa được khởi tạo.");
+                    }
+                    break;
+                case 7:
+                    // Thống kê giá trị m
+                    if (arr != null) {
+                        System.out.print("Nhập giá trị số nguyên (m): ");
+                        int m = scanner.nextInt();
+                        int count = countOccurrences(arr, m);
+                        System.out.println("Trong mảng có " + count + " phần tử có giá trị là " + m);
+                    } else {
+                        System.out.println("Mảng chưa được khởi tạo.");
+                    }
+                    break;
+                case 8:
+                    // Thoát
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Lựa chọn không hợp lệ. Vui lòng chọn lại.");
+                    break;
+            }
+        }
+    }
+
+    // Kiểm tra số nguyên tố
+    private static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Đảo ngược mảng
+    private static void reverseArray(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left < right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+    }
+
+    // Đếm số lần xuất hiện của m trong mảng
+    private static int countOccurrences(int[] arr, int m) {
+        int count = 0;
+        for (int i : arr) {
+            if (i == m) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
---
+++ src/rikkeiacademy/Session04/Weekends_ConsoleApp2.java
@@ -0,0 +1,179 @@
+package rikkeiacademy.Session04;
+
+import java.util.Scanner;
+
+public class Weekends_ConsoleApp2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Khai báo biến mảng 2 chiều
+        int[][] matrix = null;
+        int rows = 0;
+        int cols = 0;
+
+        while (true) {
+            System.out.println("***********************MENU************************");
+            System.out.println("1. Nhập giá trị các phần tử mảng 2 chiều");
+            System.out.println("2. In giá trị các phần tử theo ma trận");
+            System.out.println("3. In giá trị các phần tử nằm trên đường biên của ma trận và tính tổng");
+            System.out.println("4. In giá trị các phần tử nằm trên đường chéo chính và chéo phụ và tính tổng");
+            System.out.println("5. In ra giá trị lớn nhất và nhỏ nhất trong mảng");
+            System.out.println("6. Sắp xếp mảng tăng dần theo cột");
+            System.out.println("7. Thoát");
+            System.out.println("**************************************************");
+            System.out.print("Nhập lựa chọn của bạn: ");
+
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    // Nhập giá trị các phần tử mảng 2 chiều
+                    System.out.print("Nhập số dòng (row): ");
+                    rows = scanner.nextInt();
+                    System.out.print("Nhập số cột (col): ");
+                    cols = scanner.nextInt();
+                    matrix = new int[rows][cols];
+                    for (int i = 0; i < rows; i++) {
+                        for (int j = 0; j < cols; j++) {
+                            System.out.print("Nhập giá trị cho phần tử [" + i + "][" + j + "]: ");
+                            matrix[i][j] = scanner.nextInt();
+                        }
+                    }
+                    break;
+                case 2:
+                    // In giá trị các phần tử theo ma trận
+                    if (matrix != null) {
+                        System.out.println("Giá trị các phần tử theo ma trận:");
+                        printMatrix(matrix, rows, cols);
+                    } else {
+                        System.out.println("Mảng chưa được khởi tạo.");
+                    }
+                    break;
+                case 3:
+                    // In giá trị các phần tử nằm trên đường biên và tính tổng
+                    if (matrix != null) {
+                        int sumBoundary = sumBoundaryElements(matrix, rows, cols);
+                        System.out.println("Tổng các phần tử nằm trên đường biên: " + sumBoundary);
+                    } else {
+                        System.out.println("Mảng chưa được khởi tạo.");
+                    }
+                    break;
+                case 4:
+                    // In giá trị các phần tử nằm trên đường chéo và tính tổng
+                    if (matrix != null) {
+                        int sumDiagonals = sumDiagonalElements(matrix, rows, cols);
+                        System.out.println("Tổng các phần tử nằm trên đường chéo chính và chéo phụ: " + sumDiagonals);
+                    } else {
+                        System.out.println("Mảng chưa được khởi tạo.");
+                    }
+                    break;
+                case 5:
+                    // In giá trị lớn nhất và nhỏ nhất
+                    if (matrix != null) {
+                        int max = findMaxValue(matrix, rows, cols);
+                        int min = findMinValue(matrix, rows, cols);
+                        System.out.println("Giá trị lớn nhất trong mảng: " + max);
+                        System.out.println("Giá trị nhỏ nhất trong mảng: " + min);
+                    } else {
+                        System.out.println("Mảng chưa được khởi tạo.");
+                    }
+                    break;
+                case 6:
+                    // Sắp xếp mảng tăng dần theo cột
+                    if (matrix != null) {
+                        sortMatrixByColumn(matrix, rows, cols);
+                        System.out.println("Mảng sau khi sắp xếp tăng dần theo cột:");
+                        printMatrix(matrix, rows, cols);
+                    } else {
+                        System.out.println("Mảng chưa được khởi tạo.");
+                    }
+                    break;
+                case 7:
+                    // Thoát
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Lựa chọn không hợp lệ. Vui lòng chọn lại.");
+                    break;
+            }
+        }
+    }
+
+    // In mảng 2 chiều
+    private static void printMatrix(int[][] matrix, int rows, int cols) {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // Tính tổng các phần tử nằm trên đường biên của ma trận
+    private static int sumBoundaryElements(int[][] matrix, int rows, int cols) {
+        int sum = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (i == 0 || i == rows - 1 || j == 0 || j == cols - 1) {
+                    sum += matrix[i][j];
+                }
+            }
+        }
+        return sum;
+    }
+
+    // Tính tổng các phần tử nằm trên đường chéo chính và chéo phụ của ma trận
+    private static int sumDiagonalElements(int[][] matrix, int rows, int cols) {
+        int sum = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (i == j || i + j == rows - 1) {
+                    sum += matrix[i][j];
+                }
+            }
+        }
+        return sum;
+    }
+
+    // Tìm giá trị lớn nhất trong mảng 2 chiều
+    private static int findMaxValue(int[][] matrix, int rows, int cols) {
+        int max = matrix[0][0];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] > max) {
+                    max = matrix[i][j];
+                }
+            }
+        }
+        return max;
+    }
+
+    // Tìm giá trị nhỏ nhất trong mảng 2 chiều
+    private static int findMinValue(int[][] matrix, int rows, int cols) {
+        int min = matrix[0][0];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] < min) {
+                    min = matrix[i][j];
+                }
+            }
+        }
+        return min;
+    }
+
+    // Sắp xếp mảng theo cột
+    private static void sortMatrixByColumn(int[][] matrix, int rows, int cols) {
+        for (int j = 0; j < cols; j++) {
+            for (int i = 0; i < rows - 1; i++) {
+                for (int k = i + 1; k < rows; k++) {
+                    if (matrix[i][j] > matrix[k][j]) {
+                        int temp = matrix[i][j];
+                        matrix[i][j] = matrix[k][j];
+                        matrix[k][j] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
+
